cd24816f9a2f7b5405ddaa275548c3903afdefe9
Debashish Sinha <debashishsinha555@gmail.com>
2024-10-08 05:02:19 +0530

    Add files via upload

---
+++ if_else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d" , &n);
+    if(n%2==0)
+        printf("%d is a Even Number \n", n);
+    else
+        printf("%d is a Odd Number \n", n);
+    return 0;
+}

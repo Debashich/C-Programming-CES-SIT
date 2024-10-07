739df37d56d21737b0bcc5180ba3b9c1688ff5d3
Debashish Sinha <debashishsinha555@gmail.com>
2024-10-08 04:20:18 +0530

    Add files via upload

---
+++ var_init.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+    int A = 12;
+    float B = 23.43;
+    char C = 'A';
+    printf("A = %d \n", A);
+    printf("B = %f \n", B);
+    printf("C = %c \n", C);
+}
